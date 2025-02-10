28e3ff36b89f71c2a79c80f7a3eaff16e378e29c
Ahmad El-Mowafy <ahmadelmowafy4@gmail.com>
2025-02-10 22:46:48 +0000

    Completed typescript-modules

---
+++ typescript-modules/my-app/src/Body.tsx
@@ -0,0 +1,13 @@
+import TitleSection from './TitleSection';
+import MidTextSection from './MidTextSection';
+import BottomTextSection from './BottomTextSection';
+
+export default function Body() {
+  return (
+    <>
+      <TitleSection />
+      <MidTextSection />
+      <BottomTextSection />
+    </>
+  );
+}
